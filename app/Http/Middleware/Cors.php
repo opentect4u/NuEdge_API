01d3f2432119781a4d58bf3b9c895513fb9b4d0f
Chittaranjan Maity <opentech4u@gmail.com>
2023-01-20 15:17:10 +0530

    cors_error_fix_4_20_01_2023

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,7 @@ class Cors
     {
         header("Access-Control-Allow-Origin: *");
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
         return $next($request);
        
     }
