456e73708337de597b75cf49beb79ebc9e94a1ff
Chittaranjan Maity <opentech4u@gmail.com>
2023-01-20 15:41:52 +0530

    cors_error_fix_4_20_01_2023

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,26 @@ return [
     |
     */
 
-    'paths' => [],   
-     'allowed_methods' => ['*'],   
-      'allowed_origins' => ['*'], 
-         'allowed_origins_patterns' => [],   
-          'allowed_headers' => ['*'],   
-           'exposed_headers' => false,  
-             'max_age' => false,    
-             'supports_credentials' => false,
+    'paths' => [],
 
+    
+    'allowed_methods' => ['*'],
+
+    
+    'allowed_origins' => ['*'],
+
+   
+    'allowed_origins_patterns' => [],
+
+   
+    'allowed_headers' => ['*'],
+
+  
+    'exposed_headers' => [],
+
+   
+    'max_age' => 0,
+
+    
+    'supports_credentials' => false,
 ];
