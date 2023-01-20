db543e09d9371d85637d1990622c7dc58721d0e2
Chittaranjan Maity <opentech4u@gmail.com>
2023-01-20 14:15:23 +0530

    cors_error_fix_1_20_01_2023

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*','api/v1/*','*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*','https://nuedge.opentech4u.co.in/'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['*','https://nuedge.opentech4u.co.in/'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
