aa6a163aff6676124bdaa5be97a635e555f7085e
Chittaranjan Maity <opentech4u@gmail.com>
2023-01-20 14:58:01 +0530

    cors_error_fix_4_20_01_2023

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*','https://nuedge.opentech4u.co.in'],
 
     'allowed_origins_patterns' => [],
 
