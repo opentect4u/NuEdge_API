17a68e33cd19eeeeb25386875aaa413fd6fcc18c
Chittaranjan Maity <opentech4u@gmail.com>
2023-07-21 18:30:41 +0530

    dev3_21_07_2023

--- app/Http/Controllers/v1/Operation/TransactionDetailsController.php
+++ app/Http/Controllers/v1/Operation/TransactionDetailsController.php
@@ -210,7 +210,7 @@ class TransactionDetailsController extends Controller
                     ->leftJoin('md_subcategory','md_subcategory.id','=','md_scheme.subcategory_id')
                     ->leftJoin('md_amc','md_amc.amc_code','=','td_mutual_fund_trans.amc_code')
                     ->select('td_mutual_fund_trans.*','md_scheme.scheme_name as scheme_name','md_category.cat_name as cat_name','md_subcategory.subcategory_name as subcat_name','md_amc.amc_short_name as amc_name')
-                    ->take(10)
+                    // ->take(10)
                     ->get();
             }
         } catch (\Throwable $th) {
@@ -221,5 +221,5 @@ class TransactionDetailsController extends Controller
     }
 
 
-    
+
 }
