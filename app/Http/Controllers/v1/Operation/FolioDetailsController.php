ec4a538d76279b21dff4227917d2f0440ca84947
Chittaranjan Maity <opentech4u@gmail.com>
2023-12-08 18:09:01 +0530

    dev2_08_12_2023

--- app/Http/Controllers/v1/Operation/FolioDetailsController.php
+++ app/Http/Controllers/v1/Operation/FolioDetailsController.php
@@ -41,9 +41,12 @@ class FolioDetailsController extends Controller
             $kyc_status=$request->kyc_status;
             $nominee_status=$request->nominee_status;
             $adhaar_pan_link_status=$request->adhaar_pan_link_status;
+            $client_name=$request->client_name;
 
             $rawQuery='';
-            if ($folio_status || $pan_no || $folio_no || $kyc_status || $nominee_status || $adhaar_pan_link_status) {
+            if ($folio_status || $client_name || $pan_no || $folio_no || $kyc_status || $nominee_status || $adhaar_pan_link_status) {
+                $queryString='tt_folio_details_reports.first_client_name';
+                $rawQuery.=Helper::WhereRawQuery($client_name,$rawQuery,$queryString);
                 $queryString='tt_folio_details_reports.pan';
                 $rawQuery.=Helper::WhereRawQuery($pan_no,$rawQuery,$queryString);
                 $queryString='tt_folio_details_reports.folio_no';
