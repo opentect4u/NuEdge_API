aeb33d6d2a4513efa55ed3ea7ca8fb1fb7893d0e
Chittaranjan Maity <opentech4u@gmail.com>
2023-12-27 14:45:45 +0530

    dev2_27_12_2023

--- app/Http/Controllers/v1/Master/MailBackController.php
+++ app/Http/Controllers/v1/Master/MailBackController.php
@@ -1111,7 +1111,7 @@ class MailBackController extends Controller
                             'country'=>$value[13],
                             'tpin'=>$value[14],
                             'f_name'=>$value[16],
-                            'dob'=>$mydob,
+                            'dob'=>DATE_FORMAT($mydob,"Y-m-d"),
                             'm_name'=>$value[17],
                             'phone_residence'=>$value[18],
                             'phone_res_1'=>$value[19],
