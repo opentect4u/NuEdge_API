fa4a35863f51a945eb650ca518157e2ccbd2eb0f
Chittaranjan Maity <opentech4u@gmail.com>
2024-01-17 15:29:34 +0530

    dev5_17_01_2024

--- app/Http/Controllers/v1/Reports/MonthlyMisController.php
+++ app/Http/Controllers/v1/Reports/MonthlyMisController.php
@@ -257,7 +257,7 @@ class MonthlyMisController extends Controller
                             ->groupByRaw('IF(substr(trxn_nature,1,19)="Systematic-Reversed","Systematic-Reversed",trxn_nature)')
                             ->groupBy('td_mutual_fund_trans.trans_desc')
                             ->groupBy('td_mutual_fund_trans.kf_trans_type')
-                            ->take(10)
+                            // ->take(10)
                             ->get();
                         // dd(DB::getQueryLog());
 
